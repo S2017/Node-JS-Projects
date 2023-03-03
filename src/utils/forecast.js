54e774f6cce44f1421f00a68aab160c357def45a
Siaha <crazysai0104@gmail.com>
2023-03-03 15:10:13 +0100

    Adding new information to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if(body.error){
             callback("Unable to find location", undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] +". It is currently " + body.current.temperature + " degrees out.It feels like "+body.current.feelslike+".There is " +body.current.precip+ "% chance of rain")
+            callback(undefined, "Humidity is " +body.current.humidity + ". Cloud Cover: " +body.current.cloudcover + "." +body.current.weather_descriptions[0] +". It is currently " + body.current.temperature + " degrees out.It feels like "+body.current.feelslike+".There is " +body.current.precip+ "% chance of rain")
         }
     })
  }
